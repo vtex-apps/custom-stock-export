a005358405e2799f45681bbe830ba0f219d8bb8c
Your Name <bonacchigerman@gmail.com>
2021-12-10 10:55:27 -0300

    update exportBody

--- custom-stock-export-back/node/interfaces/exportBody.ts
+++ custom-stock-export-back/node/interfaces/exportBody.ts
@@ -1,11 +1,20 @@
 export interface ExportBodyType {
   categoryId: number
-  productId: {
-    value: string
-    operator: string
-  }
-  productName: {
-    value: string
-    operator: string
-  }
+  productId: ProductFilterType
+  productName: ProductFilterType
+  warehouseIds: string[]
+  quantity: QuantityFilterType
+  reservedQuantity: QuantityFilterType
+  availableQuantity: QuantityFilterType
+}
+
+interface ProductFilterType {
+  value: string
+  operator: string
+}
+
+interface QuantityFilterType {
+  min: number
+  max: number
+  operator: string
 }
